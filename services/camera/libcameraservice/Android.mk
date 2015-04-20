f2bb3035e05f3c01739b3a4089a6d9999482d8c3
jgcaap <jgcaap@gmail.com>
2015-04-20 07:07:31 -0400

    Revert "Compile with GCC 4.8 camera libs"
    This reverts commit f0b6b923df0a8b5f5f1e64b9e37e4a37cf762081.

--- services/camera/libcameraservice/Android.mk
+++ services/camera/libcameraservice/Android.mk
@@ -82,7 +82,3 @@ endif
 LOCAL_MODULE:= libcameraservice
 
 include $(BUILD_SHARED_LIBRARY)
-
-LOCAL_CC := $$(TARGET_TOOLCHAIN_ROOT)/../arm-linux-androideabi-4.8/bin/arm-linux-androideabi-gcc$$(HOST_EXECUTABLE_SUFFIX)
-LOCAL_CXX := $$(TARGET_TOOLCHAIN_ROOT)/../arm-linux-androideabi-4.8/bin/arm-linux-androideabi-g++$$(HOST_EXECUTABLE_SUFFIX)
-
